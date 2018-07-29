84a321b2af1f3e5b21220f7bee4871d0171a0def
ConanZhang <zhangconancomputer@126.com>
2018-07-29 14:31:33 +0800

    初始化工程

---
+++ src/main/java/com/alibaba/dubbo/cache/CacheFactory$Adaptive.java
@@ -0,0 +1,19 @@
+package com.alibaba.dubbo.cache;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+
+/**
+ * @author zkn
+ * @date 2018/7/21 14:47
+ **/
+public class CacheFactory$Adaptive implements com.alibaba.dubbo.cache.CacheFactory {
+    public com.alibaba.dubbo.cache.Cache getCache(com.alibaba.dubbo.common.URL arg0) {
+        if (arg0 == null) throw new IllegalArgumentException("url == null");
+        com.alibaba.dubbo.common.URL url = arg0;
+        String extName = url.getParameter("cache", "lru");
+        if (extName == null)
+            throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.cache.CacheFactory) name from url(" + url.toString() + ") use keys([cache])");
+        com.alibaba.dubbo.cache.CacheFactory extension = (com.alibaba.dubbo.cache.CacheFactory) ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.cache.CacheFactory.class).getExtension(extName);
+        return extension.getCache(arg0);
+    }
+}
